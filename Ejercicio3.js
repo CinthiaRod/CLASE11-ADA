beee6eec906b49dfe86c956f9a5ad38ba66e235c
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-21 21:12:06 -0800

    Descripción del ejercicio

---
+++ Ejercicio3.js
@@ -0,0 +1,5 @@
+//Ejercicio 3: ¿Sabes contar?
+//Consigna: Crea un programa que le pida al usuario un número positivo.
+// El programa deberá contar desde el 1 hasta ese número e imprimir cada valor en la consola.
+// Usa un ciclo for para realizar la tarea.
+
