ba91f46d6c489ceb91dfda812ad00579ec67605d
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-25 20:50:34 -0800

    Código del ejercicio

--- Ejercicio6.js
+++ Ejercicio6.js
@@ -3,3 +3,16 @@
 //luego imprima su tabla de multiplicar desde el 1 hasta el 10. 
 //Usa un ciclo for.
 
+const prompt =require('prompt-sync')();
+
+let Number = parseInt (prompt("Enter a number: "));
+
+if (!isNaN(Number)){
+console.log ('This is the multiplication table of the number', Number);
+for ( let Lap = 1; Lap <=10; Lap ++){
+console.log (`${Number} x ${Lap} = ${Number * Lap}`);   
+}
+} else {
+console.log ('Invalid number')
+}
+
