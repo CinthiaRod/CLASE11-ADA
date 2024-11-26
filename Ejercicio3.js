f5031de20e7d7be2aa58f4d242fc7577ed5e8243
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-25 19:24:42 -0800

    Código del ejercicio

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -3,3 +3,15 @@
 // El programa deberá contar desde el 1 hasta ese número e imprimir cada valor en la consola.
 // Usa un ciclo for para realizar la tarea.
 
+const prompt = require('prompt-sync')();
+
+let PositiveNumber = parseInt(prompt("Enter a number positive: "));
+
+if( PositiveNumber >= 1){
+for(let Lap = 1; Lap <= PositiveNumber; Lap++)
+console.log ('Turning around number' + Lap);
+}else{
+    console.log ('Invalid number')
+
+}
+
