8970e7af6bddf2f0aff2f199aca8705f7b8b643d
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-25 21:43:04 -0800

    Código del ejercicio

--- Ejercicio9.js
+++ Ejercicio9.js
@@ -4,3 +4,10 @@
 //imprimir "¡Despegue!" al final. 
 //Usa un ciclo for para realizar la cuenta regresiva.
 
+const prompt = require('prompt-sync')();
+
+console.log('Start countdown')
+for( let Countdown = 10; Countdown >= 0; Countdown--){
+console.log (Countdown);
+}
+console.log ('Takeoff!');
